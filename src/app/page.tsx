81c378a69319d505bf6f25ba9253801fae83cfc2
Mikaelwendin <mikael.wendin83@gmail.com>
2024-03-19 14:13:27 +0100

    added embla carousel and more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Information from "./components/information";
 import ScrollIndicator from "./components/scrollindicator";
 
 const Home = () => {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+    const scrollPrev = useCallback(() => {
+        if (emblaApi) emblaApi.scrollPrev();
+    }, [emblaApi]);
+
+    const scrollNext = useCallback(() => {
+        if (emblaApi) emblaApi.scrollNext();
+    }, [emblaApi]);
+
     const presentation = {
-        title: "Dynamic Title 1",
-        description: "This is some dynamic content 1.",
+        title: "Dynamic Title 2",
+        description: "This is some dynamic content 2.",
     };
     const info = {
         title: "Dynamic Title 1",
         description: "This is some dynamic content 1.",
+    };
+    const info2 = {
+        title: "hej",
+        description: "This hej.",
+    };
+    const presentation2 = {
+        title: "tjena",
+        description: "lol.",
+    };
+    const info3 = {
+        title: "asdfasfd",
+        description: "asdfasdfsadf hej.",
+    };
+    const presentation3 = {
+        title: "sadfasdfasdf",
+        description: "loasdfasdl.",
     };
 
     useEffect(() => {
@@ -34,7 +61,7 @@ const Home = () => {
         };
 
         const observer = new IntersectionObserver(handleIntersect, options);
-        const informationContainers = document.querySelectorAll(".information-container");
+        const informationContainers = document.querySelectorAll(".embla");
 
         informationContainers.forEach((container) => {
             observer.observe(container);
@@ -57,17 +84,29 @@ const Home = () => {
             </div>
             <main className="relative">
                 <div className="main-content">
-                    <div className="information-container">
-                        <Information content={presentation} />
-                        <Information content={info} />
-                    </div>
-                    <div className="information-container">
-                        <Information content={presentation} />
-                        <Information content={info} />
-                    </div>
-                    <div className="information-container">
-                        <Information content={presentation} />
-                        <Information content={info} />
+                    <div className="embla" ref={emblaRef}>
+                        <div className="embla__container">
+                            <div className="embla__slide information-container" key="1">
+                                <Information content={presentation} />
+                                <Information content={info} />
+                            </div>
+                            <div className="embla__slide information-container" key="2">
+                                <Information content={presentation2} />
+                                <Information content={info2} />
+                            </div>
+                            <div className="embla__slide information-container" key="3">
+                                <Information content={presentation3} />
+                                <Information content={info3} />
+                            </div>
+                        </div>
+                        <div className="button-container">
+                            <button className="embla__prev btn" onClick={scrollPrev}>
+                                Prev
+                            </button>
+                            <button className="embla__next btn" onClick={scrollNext}>
+                                Next
+                            </button>
+                        </div>
                     </div>
                 </div>
             </main>
